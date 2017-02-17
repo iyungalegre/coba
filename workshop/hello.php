4d941bec8a08f022e83cfac9405986428ddcfa9f
iyungalegre <muhammad_chairul@outlook.com>
2017-02-17 17:47:04 +0700

    awal
    awal

---
+++ workshop/hello.php
@@ -0,0 +1,28 @@
+<html>
+	<head>
+		<title>
+			Ini Judul Aplikasi
+		</title>
+	</head>
+	<body>
+		<u><h2 align="center">Judul Table</h2></u>
+		<table border="1" width="100%">
+			<thead>
+				<tr>
+					<td width="10%">No</td>
+					<td>Nama</td>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>1.</td>
+					<td>Meja Tulis</td>
+				</tr>
+				<tr>
+					<td>2.</td>
+					<td>Kursi</td>
+				</tr>
+			</tbody>
+		</table>
+	</body>
+</html>
---
+++ workshop/index.php
@@ -0,0 +1,28 @@
+<html>
+	<head>
+		<title>
+			Ini Judul Aplikasi
+		</title>
+	</head>
+	<body>
+		<table border="1" width="100%" background="">
+			<tr>
+				<td><h2 align="center">Ini Judul</h2></td>
+			</tr>
+			<tr height="630px">
+				<td>
+					<table border="1" width="100%" background="">
+						<tr height="650px">
+							<td width="250px">
+								Ini Menu
+							</td>
+							<td>
+								Ini Isi
+							</td>
+						</tr>
+					</table>
+				</td>
+			</tr>
+		</table>
+	</body>
+</html>
